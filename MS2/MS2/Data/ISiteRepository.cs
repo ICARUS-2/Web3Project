c5a56d89a2c2cdb47bbf4743b3914cb790490133
Aidan-Eves <eves.aidan@gmail.com>
2021-12-01 22:54:40 -0500

    code clean up

--- MS2/MS2/Data/ISiteRepository.cs
+++ MS2/MS2/Data/ISiteRepository.cs
@@ -9,7 +9,6 @@ namespace MS2.Data
         IEnumerable<Product> GetProductsByCategory(string category);
         IEnumerable<Order> GetAllOrders();
         bool SaveAll();
-        public IEnumerable<OrderEntry> GetShoppingCartItems();
         public IEnumerable<JobPosting> GetAllJobPostings();
         public void InsertOrder(Order order);
         public void InsertOrderEntry(OrderEntry orderEntry);
